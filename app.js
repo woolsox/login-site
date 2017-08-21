3ff60f6cc8e56d16eb2a83ab0123b7fa0ff7c70b
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-21 12:29:20 -0400

    fourth commit - got user input logging, but nothing to verify yet

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const users = require('./users.js')
 const express = require('express');
-const expressSession = require('express-session')
+const session = require('express-session')
+const bodyParser = require('body-parser');
 const mustache = require('mustache-express');
 const port = 3000;
 
@@ -8,9 +10,19 @@ const app = express();
 app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.get('/login', function(req, res){
- res.render('login');
+ res.render('login', users);
+});
+
+app.post('/login', function(req, res){
+ let username = req.body.username;
+ let password = req.body.password;
+ // users.push(test);
+ console.log(username + password);
 });
 
 app.listen(port, function(req, res){
---
+++ users.js
@@ -0,0 +1,8 @@
+module.exports = {
+ users: [{
+   "colin": "dimeo",
+   "brett": "shirley",
+   "tivona": "spruilli",
+   "katy": "dillard"
+ }]
+}
