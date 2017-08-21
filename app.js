648d8a1b92bc0c7904bd18c4401ace0750cce5f8
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-21 11:04:12 -0400

    third commit - login page now rendering

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const expressSession = require('express-session')
-const mustacheExpress = require('mustache-express');
-const mustache = require('mustache')
+const mustache = require('mustache-express');
+const port = 3000;
+
+const app = express();
+
+app.engine('mustache', mustache());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.get('/login', function(req, res){
+ res.render('login');
+});
+
+app.listen(port, function(req, res){
+ console.log('Starting express-session login app...');
+});
